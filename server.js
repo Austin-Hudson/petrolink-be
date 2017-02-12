37a0b4f3847dc997e950ce7db59ed52fbb4c795b
Austin Hudson <arhudson7@gmail.com>
2017-02-12 08:13:05 -0600

    fixed the server so clients can see what gets drawn

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 var port = process.env.PORT || 3000;
-
-// app.get('/', function(req, res){
-//   res.sendfile('/index.html');
-// });
+var drawArray = [];   //for storing what has been drawn so far
 
 io.on('connection', function(socket){
 
@@ -16,7 +13,18 @@ io.on('connection', function(socket){
   //user has disconnected
   socket.on('disconnect', function(){
     io.emit('user-disconnect', {message: "a user disconnected"});
- });
+  });
+
+ //update new client with current drawing that has been drawn
+   socket.emit('draw-current', {array: drawArray});
+
+  //client has drawn something
+  socket.on('draw-new', function(data){
+    drawArray.push(data.line);
+    //send line to clients
+    socket.broadcast.emit('draw-new', {line: data.line});
+  });
+
 });
 
 server.listen(port, function(){
