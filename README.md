01a1d9b3110794664828a410f46b7320eae7557d
Austin Hudson <arhudson7@gmail.com>
2017-02-13 09:02:07 -0600

    added more handlers to communicate when the user picks specific tools. Edited README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# petrolink-be
+# Petrolink Back-end
+
+### How to install
+1. `npm install` in root of directory
+2. `node server.js`
--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var io = require('socket.io').listen(server);
 
 var port = process.env.PORT || 3000;
 var drawArray = [];   //for storing what has been drawn so far
+var lineArray = [];
+var squareArray = [];
+var circleArray = [];
+var triangleArray = [];
 
 io.on('connection', function(socket){
 
@@ -16,7 +20,16 @@ io.on('connection', function(socket){
   });
 
  //update new client with current drawing that has been drawn
-   socket.emit('draw-current', {array: drawArray});
+  socket.emit('draw-current-drawing', {draw: drawArray});
+ // update new client with lines that have been drawn;
+  socket.emit('draw-current-line', {line: lineArray});
+  // update new client with squares that have been drawn
+  socket.emit('draw-current-square', {square: squareArray});
+  // update new client with circles that have been drawn
+  socket.emit('draw-current-circle', {circle: circleArray});
+  // update new client with triangle that have been drawn
+  socket.emit('draw-current-triangle', {triangle: triangleArray});
+
 
   //client has drawn something
   socket.on('draw-new', function(data){
@@ -24,6 +37,30 @@ io.on('connection', function(socket){
     //send line to clients
     socket.broadcast.emit('draw-new', {line: data.line});
   });
+
+  //client has drawn a line
+  socket.on('draw-new-line', function(data){
+    lineArray.push(data.line);
+    socket.broadcast.emit('draw-new-line', {line: data.line});
+  });
+
+  //client has drawn a square
+  socket.on('draw-new-square', function(data){
+    squareArray.push(data.square);
+    socket.broadcast.emit('draw-new-square', {square: data.square});
+  });
+
+  //client has drawn a circle
+  socket.on('draw-new-circle', function(data){
+    circleArray.push(data.circle);
+    socket.broadcast.emit('draw-new-circle', {circle: data.circle});
+  });
+
+  //client has drawn a triangle
+  socket.on('draw-new-triangle', function(data){
+    triangleArray.push(data.triangle);
+    socket.broadcast.emit('draw-new-triangle', {triangle: data.triangle})
+  })
 
 });
 
